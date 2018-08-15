ae7a344faad2dc4c8e2f513d505ac479a534f2b0
JiangSheng <hzjiangsheng1@corp.netease.com>
2018-08-15 12:49:34 +0800

    add webproject

---
+++ 01/static/js/index.js
@@ -0,0 +1,28 @@
+const getJsonDataUrl = 'jsonData';
+const downloadUrl = 'download';
+
+
+// 原生JS 调用Ajax
+document.addEventListener('readystatechange', function(){
+	if (document.readyState == 'complete'){
+		document.getElementById('jsonButton').addEventListener('click', function() {
+			// Ajax 请求
+			let xmlHttp = new XMLHttpRequest();
+			xmlHttp.onreadystatechange = function() {
+				if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+					// 解析返回值
+					JSON.parse(xmlHttp.responseText);
+				}
+			}
+			xmlHttp.open("GET", getJsonDataUrl, true);
+			xmlHttp.send();
+		});
+	}	
+});
+
+// jQuery 调用 Ajax
+$(document).ready(function() {
+	$('#downlodButton').click(function() {
+		window.open(downloadUrl);
+	});
+});
