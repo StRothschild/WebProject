4b4786bdca447853e9a8d475fbf37b2d8c3a5390
JiangSheng <hzjiangsheng1@corp.netease.com>
2018-08-29 19:49:45 +0800

    完善webpack

--- 01/webpack.config.js
+++ 01/webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = {
     // 输入
     entry: path.resolve('webpackMain.js'),       // path.resolve 返回一个以相对于当前的工作目录的绝对路径  
-    
+
     // 输出
     output: {
         path: path.join(__dirname, '/dist'),     // path.join 将参数组合成一个路径并返回    __dirname 是 Node.js 全局对象上的一个熟悉代表当前文件的绝对路径  
@@ -16,7 +16,7 @@ module.exports = {
         rules: [
             // 在 require() 或 import 语句中解析 '.txt' 的路径时，先使用 raw-loader 转换一下
             { 
-                test: /\.txt$/, 
+                test: /\.txt$/,
                 use: 'raw-loader' 
             }
         ]
