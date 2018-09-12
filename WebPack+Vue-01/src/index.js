bcb829fa6b3db510d8a9b8f1c5704ac044541206
StRothschild <463215040@qq.com>
2018-09-12 21:01:22 +0800

    新增webpack+vue工程

---
+++ WebPack+Vue-01/src/index.js
@@ -0,0 +1,39 @@
+// 导入 Vue
+import vue from 'Vue';
+// 导入 lodash
+import _ from 'lodash';
+// 导入 css 文件
+import './css/index.css';
+// 导入 style 文件
+import './css/stylus/index.styl';
+// 导入也可以用 require 语法
+//require('./css/stylus/index.styl');
+
+// 导入 gif 文件
+import pic from './h5.gif';
+// 导入 js 文件
+import printLog from './printfn.js';
+
+
+
+
+
+
+
+// 调用 init 方法
+// document.body.appendChild(init());
+// function init() {
+//     // 新增 div
+//     let element = document.createElement('div');
+//     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//     // 将图像添加到 div
+//     let myPic = new Image();
+//     myPic.src = pic;
+//     element.appendChild(myPic);
+
+//     return element;
+// }
+
+// // 调用 printLog 方法
+// printLog();
